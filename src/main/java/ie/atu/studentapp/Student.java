62449a9f1845ab6a1a61ccbb950a189d727f183c
G00394277 <G00394277@atu.ie>
2024-10-21 12:46:59 +0100

    Feature commit

--- src/main/java/ie/atu/studentapp/Student.java
+++ src/main/java/ie/atu/studentapp/Student.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.lang.annotation.Documented;
 
 @Data
-@Document (Collection = Student);
 public class Student {
     private String studentId;
     private String firstName;
